06d6aac1b87b202299ca03118a66fe7bb672936c
KC1064 <kiron08072003@gmail.com>
2024-07-11 01:05:32 +0530

    Updated: Completed Header for mobile view

---
+++ Project_01/landing-page/src/App.jsx
@@ -0,0 +1,16 @@
+import Body from "./components/Body"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <div className="bg-amber-100 min-h-svh">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ Project_01/landing-page/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaBars } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div className='flex justify-between px-1 py-2.5'>
+        <div className='flex gap-4 px-1'>
+            <img src="./assets/Logo.svg" alt="logo" className=''/>
+            <button className='bg-gradient-to-r from-orange-400 to-red-400 px-1.5 py-1 text-xs rounded-2xl text-white'>Hosterr is hiring</button>
+        </div>
+        <div className='px-3 align-middle py-1'>
+        <FaBars className='size-4.5'/>
+        </div>
+    </div>
+  )
+}
+
+export default Header
