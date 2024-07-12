67802e673c791b5351bfc871ce3fb72708187c18
KC1064 <kiron08072003@gmail.com>
2024-07-12 15:54:57 +0530

    Started new Project

---
+++ EdTech-Landing-Page/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header"
+import Intro from "./components/Intro"
+import Testimonials from "./components/Testimonials"
+import Pricing from "./components/Pricing"
+import Footer from "./components/Footer"
+import Features from "./components/Features"
+
+function App() {
+
+  return (
+    <div>
+      <Header></Header>
+      <Intro></Intro>
+      <Testimonials></Testimonials>
+      <Pricing></Pricing>
+      <Features></Features>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
