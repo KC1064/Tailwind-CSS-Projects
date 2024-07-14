b25221de1e7344ac71d59be482fcc16dfd92e259
KC1064 <kiron08072003@gmail.com>
2024-07-14 16:10:11 +0530

    Minor changes in padding and button placements

--- EdTech-Landing-Page/src/components/Header.jsx
+++ EdTech-Landing-Page/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full md:flex md:py-4 md:px-5 md:items-center md:justify-center lg:px-8">
+    <div className="w-full px-2 md:flex md:py-4 md:px-5 md:items-center md:justify-center lg:px-8">
       <div className="flex justify-between w-[100%] md:h-auto md:items-center">
         <img src="./images/logo.svg" alt="logo" className="h-10 w-auto md:h-12 flex md:items-center" />
         <div className="hidden md:flex md:items-center md:justify-center md:bg-gray-100 md:rounded-full">
@@ -11,7 +11,7 @@ const Header = () => {
         <a href="" className="hover:bg-white md:rounded-full px-2 py-1.5 font-poppins text-sm">Testimonials</a>
         <a href="" className="hover:bg-white md:rounded-full px-2 py-1.5 font-poppins text-sm">Features</a>
       </div>
-        <button className="text-sm border-none bg-black text-white rounded-full font-semibold px-3 font-spline w-[139px] hover:bg-primary lg:h-8">
+        <button className="text-sm border-none bg-black text-white rounded-full font-semibold px-3 font-spline w-[139px] hover:bg-primary lg:h-8 md:h-[40px]">
           Join Community
         </button>
       </div>
