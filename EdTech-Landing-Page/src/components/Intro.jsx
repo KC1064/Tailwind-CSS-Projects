41644d09e34e06e4974ba6f7d86d9e367b709140
KC1064 <kiron08072003@gmail.com>
2024-07-15 18:29:58 +0530

    Working on pricing page

--- EdTech-Landing-Page/src/components/Intro.jsx
+++ EdTech-Landing-Page/src/components/Intro.jsx
@@ -4,16 +4,26 @@ const Intro = () => {
   return (
     <div className="mt-16 flex flex-col gap-10 md:gap-12">
       <div className="flex flex-col gap-6">
-        <h1 className="text-6xl font-spline font-bold text-center leading-none text-primary pt-3 md:text-8xl">ATTEND <span>LIVE</span><span className="text-black"> CLASSES EVERY WEEK</span></h1>
-        <p className="text-center text-[16px] font-semibold font-poppins md:text-[20px] md:px-20 md:py-6">Join founders, freelancers, designers, FAANG engineers, indie hackers, 
-          and more in our 24/7 WhatsApp Group for less than a cup of coffee.</p>
+        <h1 className="text-6xl font-spline font-bold text-center leading-none text-primary pt-3 md:text-8xl">
+          ATTEND <span>LIVE</span>
+          <span className="text-black"> CLASSES EVERY WEEK</span>
+        </h1>
+        <p className="text-center text-[16px] font-semibold font-poppins md:text-[20px] md:px-20 md:py-6">
+          Join founders, freelancers, designers, FAANG engineers, indie hackers,
+          and more in our 24/7 WhatsApp Group for less than a cup of coffee.
+        </p>
         <div className="flex justify-center mr-4">
-        <button className="flex justify-center items-center text-lg rounded-full bg-primary ml-4 h-[80px] text-white font-poppins font-semibold
-        md:h-[60px] md:w-[60%]">Join 24/7 Whatsapp + discord for $5/month</button>
+          <button
+            className="flex justify-center items-center text-md px-6 rounded-full bg-primary ml-4 h-[80px] text-white font-poppins font-semibold
+        md:h-[60px] md:w-[60%]"
+          >
+            Join 24/7 Whatsapp + discord for $5/month
+          </button>
         </div>
       </div>
       <div className="mt-8">
-        <img src="./images/Designer (4).png" alt="" className="h-full w-auto"/>
+        <img src="./images/Designer (4).png" alt="" className="h-full w-auto" />
+        {/* Have some padding to add */}
       </div>
       <div className="flex flex-wrap justify-center items-center gap-10 mb-6">
         <div className="flex gap-2 md:gap-4 items-center">
@@ -22,11 +32,15 @@ const Intro = () => {
         </div>
         <div className="flex gap-2 md:gap-4 items-center">
           <img src="./images/icon-courses.svg" alt="" />
-          <h3 className="font-semibold font-spline">Exclusive Courses and Projects</h3>
+          <h3 className="font-semibold font-spline">
+            Exclusive Courses and Projects
+          </h3>
         </div>
         <div className="flex gap-2 md:gap-4 items-center">
           <img src="./images/icon-whatsapp.svg" alt="" />
-          <h3 className=" font-semibold font-spline">24/7 WhatsApp Support Group</h3>
+          <h3 className=" font-semibold font-spline">
+            24/7 WhatsApp Support Group
+          </h3>
         </div>
         <div className="flex gap-2 md:gap-4 items-center">
           <img src="./images/icon-sessions.svg" alt="" />
@@ -34,7 +48,8 @@ const Intro = () => {
         </div>
       </div>
     </div>
-  ); className="flex gap-4"
+  );
+  className = "flex gap-4";
 };
 
 export default Intro;
--- EdTech-Landing-Page/src/components/Pricing.jsx
+++ EdTech-Landing-Page/src/components/Pricing.jsx
@@ -1,10 +1,66 @@
-
-import React from 'react'
+import React from "react";
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <div className="flex flex-col text-center">
+      <h2 className="text-3xl font-spline font-bold leading">
+        SIMPLE NO-TRICKS PRICING
+      </h2>
+      <h4 className="py-6 px-4 font-poppins">
+        Everything you need to grow 10x for less than coffee.
+      </h4>
+      <div className="text-left border-solid border-2 border-gray-300 rounded-2xl">
+        <h2 className="pt-10 text-xl font-bold font-spline px-9">
+          Premium Elite Subscription
+        </h2>
+        <p className="pt-3 font-spline px-9">
+          Join founders, freelancers, designers, FAANG engineers, indie hackers,
+          and more in our 24/7 WhatsApp Group for less than a cup of coffee.
+        </p>
+        <p className="flex items-center gap-2 font-bold text-green-500 px-9 pt-10">
+          What's included{" "}
+          <div className="flex-auto h-[2px] bg-gray-200 mt-1"></div>
+        </p>
+        <div className="flex flex-col px-9">
+          <h3 className="flex items-center gap-2 mt-4">
+            <img src="./images/icon-check.svg" alt="" />
+            Expert Mentors
+          </h3>
+          <h3 className="flex items-center gap-2 mt-4">
+            <img src="./images/icon-check.svg" alt="" />
+            Exclusive Courses
+          </h3>
+          <h3 className="flex items-center gap-2 mt-4">
+            <img src="./images/icon-check.svg" alt="" />
+            Netwroking & Collaboration
+          </h3>
+          <h3 className="flex items-center gap-2 mt-4">
+            <img src="./images/icon-check.svg" alt="" />
+            Career Guidance
+          </h3>
+        </div>
+        <div>
+          <div className="mt-6 bg-gray-100 text-center w-[96%] ml-2 rounded-lg mb-2">
+            <h3 className="pt-6 font-poppins text-gray-500">
+              Less than a cup of coffee
+            </h3>
+            <div className="flex justify-center gap-2 items-center font-spline">
+            <h1 className="text-6xl">$5</h1> 
+            <h1 className="text-4xl line-through">$1000</h1>
+            </div>
+            <button>Join Community</button>
+            <div>
+              {/* <img src="/images/avatars/Designer (5).png" alt="" />
+            <img src="/images/avatars/Designer (6).png" alt="" />
+            <img src="/images/avatars/Designer (7).png" alt="" />
+            <img src="/images/avatars/pic-1.png" alt="" />
+            <img src="/images/avatars/pic-2.png" alt="" /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Pricing
+export default Pricing;
