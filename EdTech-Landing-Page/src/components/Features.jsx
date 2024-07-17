4cbb0dcb40c1603fbc9bbec06b29cddf9ce930b4
KC1064 <kiron08072003@gmail.com>
2024-07-17 19:45:22 +0530

    Working on features and footer sections

--- EdTech-Landing-Page/src/components/Features.jsx
+++ EdTech-Landing-Page/src/components/Features.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <div className="flex flex-col text-center px-3">
+      <h1 className="text-3xl font-spline font-bold">
+        EVERYTHING YOU NEED IN ONE COMMUNITY
+      </h1>
+      <p className="pt-5 font-poppins">
+        Unleashing your digital potential for lasting success
+      </p>
+      <div className="flex flex-col items-center text-center">
+        <img
+          src="/images/illustration-mentors.svg"
+          alt=""
+          className="w-[250px] h-[250px]"
+        />
+        <h3 className="text-xl font-spline font-bold pb-1">EXPERT MENTORS</h3>
+        <p className="font-spline leading-7">
+          Get personalized guidance from expert mentors to take your coding
+          skills to the next level.
+        </p>
+      </div>
+      <div className="flex flex-col items-center text-center">
+        <img
+          src="/images/illustration-networking.svg"
+          alt=""
+          className="w-[250px] h-[250px]"
+        />
+        <h3 className="text-xl font-spline font-bold pb-1">NETWORKING</h3>
+        <p className="font-spline leading-7">
+          Connect with like-minded coders from around the world and collaborate
+          on exciting projects.
+        </p>
+      </div>
+      <div className="flex flex-col items-center text-center">
+        <img
+          src="/images/illustration-courses.svg"
+          alt=""
+          className="w-[250px] h-[250px]"
+        />
+        <h3 className="text-xl font-spline font-bold pb-1 mt-4">COURSES</h3>
+        <p className="font-spline leading-7">
+          Exclusive courses in Full Stack, AI, Blockchain and more to help you
+          stay ahead of the curve.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Features
+export default Features;
--- EdTech-Landing-Page/src/components/Footer.jsx
+++ EdTech-Landing-Page/src/components/Footer.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="px-3">
+      <div className="flex flex-wrap justify-center items-center gap-x-6 gap-y-6 ">
+        <img
+          src="./images/company-logos/adobe.svg"
+          alt=""
+          className="h-7 w-auto"
+        />
+        <img
+          src="./images/company-logos/airbnb.svg"
+          alt=""
+          className="h-7 w-auto pr-2"
+        />
+        <img
+          src="./images/company-logos/figma.svg"
+          alt=""
+          className="h-7 w-auto"
+        />
+        <img
+          src="./images/company-logos/framer.svg"
+          alt=""
+          className="h-7 w-auto"
+        />
+        <img
+          src="./images/company-logos/google.svg"
+          alt=""
+          className="h-7 w-auto"
+        />
+        <img
+          src="./images/company-logos/dropbox.svg"
+          alt=""
+          className="h-7 w-auto"
+        />
+      </div>
+      <div>
+        <h2>GAIN A COMPETITIVE EDGE IN JOB MARKET</h2>
+        <p>
+          Join founders, freelancers, designers, FAANG engineers, indie hackers,
+          and more in or 24/7 WhatsApp Group for less than a cup of coffee.
+        </p>
+        <h3>WHAT YOU'LL GET</h3>
+        <div>
+          <img src="" alt="" />
+          <p>Comprehensive guides, projects & courses</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <p>Exclusive job + internship postings</p>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <p>Comprehensive guides, projects & courses</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
