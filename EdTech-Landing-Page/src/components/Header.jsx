fc9473745d79dc5e18fad901fe889d6d5e8a8dd6
KC1064 <kiron08072003@gmail.com>
2024-07-21 20:47:43 +0530

    Done with Intro Section

--- EdTech-Landing-Page/src/components/Header.jsx
+++ EdTech-Landing-Page/src/components/Header.jsx
@@ -2,16 +2,40 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full px-2 md:flex md:py-4 md:px-5 md:items-center md:justify-center lg:px-8">
+    <div className="w-full px-2 md:flex md:py-4 md:px-5 md:items-center md:justify-center lg:px-6">
       <div className="flex justify-between w-[100%] md:h-auto md:items-center">
-        <img src="./images/logo.svg" alt="logo" className="h-10 w-auto md:h-12 flex md:items-center" />
-        <div className="hidden md:flex md:items-center md:justify-center md:bg-gray-100 md:rounded-full">
-        <a href="" className="hover:bg-white md:rounded-full px-3 py-1.5 font-poppins text-sm">Intro</a>
-        <a href="" className="hover:bg-white md:rounded-full px-4 py-1.5 font-poppins text-sm">Pricing</a>
-        <a href="" className="hover:bg-white md:rounded-full px-2 py-1.5 font-poppins text-sm">Testimonials</a>
-        <a href="" className="hover:bg-white md:rounded-full px-2 py-1.5 font-poppins text-sm">Features</a>
-      </div>
-        <button className="text-sm border-none bg-black text-white rounded-full font-semibold px-3 font-spline w-[139px] hover:bg-primary lg:h-8 md:h-[40px]">
+        <img
+          src="./images/logo.svg"
+          alt="logo"
+          className="h-10 w-auto md:h-12 flex md:items-center lg:h-14"
+        />
+        <div className="hidden md:flex md:items-center md:justify-center md:bg-gray-100 md:rounded-full lg:h-[50px] lg:w-[400px]">
+          <a
+            href=""
+            className="hover:bg-white md:rounded-full px-3 py-1.5 font-poppins text-sm lg:text-lg lg:text-gray-400"
+          >
+            Intro
+          </a>
+          <a
+            href=""
+            className="hover:bg-white md:rounded-full px-4 py-1.5 font-poppins text-sm lg:text-lg lg:text-gray-400"
+          >
+            Pricing
+          </a>
+          <a
+            href=""
+            className="hover:bg-white md:rounded-full px-2 py-1.5 font-poppins text-sm lg:text-lg lg:text-gray-400"
+          >
+            Testimonials
+          </a>
+          <a
+            href=""
+            className="hover:bg-white md:rounded-full px-2 py-1.5 font-poppins text-sm lg:text-lg lg:text-gray-400"
+          >
+            Features
+          </a>
+        </div>
+        <button className="text-sm border-none bg-black text-white rounded-full font-semibold px-3 font-spline w-[139px] hover:bg-primary lg:h-8 md:h-[40px] lg:h-[50px] lg:w-[160px] lg:text-[16px]">
           Join Community
         </button>
       </div>
--- EdTech-Landing-Page/src/components/Intro.jsx
+++ EdTech-Landing-Page/src/components/Intro.jsx
@@ -3,28 +3,37 @@ import React from "react";
 const Intro = () => {
   return (
     <div className="mt-16 flex flex-col gap-10 md:gap-12">
-      <div className="flex flex-col gap-6">
-        <h1 className="text-6xl font-spline font-bold text-center leading-none text-primary pt-3 md:text-8xl">
-          ATTEND <span>LIVE</span>
-          <span className="text-black"> CLASSES EVERY WEEK</span>
-        </h1>
-        <p className="text-center text-[16px] font-semibold font-poppins md:text-[20px] md:px-20 md:py-6">
-          Join founders, freelancers, designers, FAANG engineers, indie hackers,
-          and more in our 24/7 WhatsApp Group for less than a cup of coffee.
-        </p>
-        <div className="flex justify-center mr-4">
-          <button
-            className="flex justify-center items-center text-md px-6 rounded-full bg-primary ml-4 h-[80px] text-white font-poppins font-semibold
-        md:h-[60px] md:w-[60%]"
-          >
+      <div className="lg:flex">
+        <div className="flex flex-col gap-y-6">
+          <h1 className="text-6xl font-spline font-bold text-center leading-none text-primary pt-3 md:text-8xl lg:text-left lg:text-9xl">
+            ATTEND <span>LIVE</span>
+            <span className="text-black"> CLASSES EVERY WEEK</span>
+          </h1>
+
+          <p className="text-center text-[16px] font-poppins md:text-[20px] md:px-20 md:py-6 lg:text-left lg:px-2">
+            Join founders, freelancers, designers, FAANG engineers, indie
+            hackers, and more in our 24/7 WhatsApp Group for less than a cup of
+            coffee.
+          </p>
+          <div className="flex justify-center mr-4 lg:justify-start lg:px-0">
+            <button
+              className="flex justify-center items-center text-md px-6 rounded-full bg-primary ml-4 h-[80px] text-white font-poppins font-semibold
+        md:h-[60px] md:w-[60%] lg:w-[70%]"
+            >
             Join 24/7 Whatsapp + discord for $5/month
-          </button>
+            </button>
+          </div>
+        </div>
+        <div className="mt-8 lg:w-[150%]">
+          <img
+            src="./images/Designer (4).png"
+            alt=""
+            className="h-full w-auto"
+          />
+          {/* Have some padding to add */}
         </div>
       </div>
-      <div className="mt-8">
-        <img src="./images/Designer (4).png" alt="" className="h-full w-auto" />
-        {/* Have some padding to add */}
-      </div>
+
       <div className="flex flex-wrap justify-center items-center gap-10 mb-6">
         <div className="flex gap-2 md:gap-4 items-center">
           <img src="./images/icon-guides.svg" alt="" />
